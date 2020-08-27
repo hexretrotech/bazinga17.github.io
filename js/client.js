a05270071361523e20fed25f5b147f9d39395bdb
bazinga17 <70258197+bazinga17@users.noreply.github.com>
2020-08-27 15:46:43 +0300

    Update client.js

--- js/client.js
+++ js/client.js
@@ -1,8 +1,20 @@
-var cors = require('cors');
-var express = require('express');
-var app = express();
+var Promise = TrelloPowerUp.Promise;
 
-app.use(cors({ origin: 'https://trello.com' }));
-
-
-console.log('Hello');
+TrelloPowerUp.initialize({
+  // Start adding handlers for your capabilities here!
+	// 'card-buttons': function(t, options) {
+	// return t.set("member", "shared", "hello", "world")
+	// .then(function(){
+	// 	  return [{
+	// icon: BLACK_ROCKET_ICON,
+	// 		  text: 'Estimate Size',
+	//       callback: function(t) {
+	//         return t.popup({
+	//           title: "Estimation",
+	//           url: 'estimate.html',
+	//         });
+	//       }
+	// 	  }];
+	// })
+	// },
+}); 
